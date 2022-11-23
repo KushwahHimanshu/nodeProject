e1235d23ad16afe12ff40ad0138102392a37805a
Himanshu <himanshukushwah95@gmail.com>
2022-11-23 22:24:35 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,62 @@
+let express = require('express');
+let app = express();
+let dotenv = require('dotenv')
+dotenv.config()
+let port = process.env.PORT || 8690;
+let morgan = require('morgan');
+let fs = require('fs');
+
+let menu = [
+    {link:'/', name:'Home'},  
+    {link:'/category', name:'Category'},
+    {link:'/products', name:'Product'},
+    {link:'/restaurants', name:'Restaurants'}
+]
+
+let categoryRoutes = require('./src/routes/categoryRoutes')
+let productRoutes = require('./src/routes/productRoutes')
+
+// save app logs in file
+app.use(morgan('common',{stream:fs.createWriteStream('./app.log')}))
+
+
+// static file path
+app.use(express.static(__dirname+'/public'))
+// html file path
+app.set('views','./src/views')
+// view engine
+app.set('view engine', 'ejs')
+
+
+let catData = [
+    {
+        "id":1,
+        "name":"Shopping",
+        "image":"https://i.ibb.co/56VP0Fn/cloths.jpg",
+        "link":"/category"
+    },
+    {
+        "id":2,
+        "name":"Restaurants",
+        "image":"https://b.zmtcdn.com/data/pictures/chains/3/6303/640252389ddc3f264dd0e9f2741e73cd.jpg",
+        "link":"/restaurants"
+    }
+]
+
+
+
+
+app.get('/',function(req,res){
+    res.render('index',{title:'AFFY INFORMATICS',data:catData,menu})
+})
+
+
+app.use('/category', categoryRoutes);
+app.use('/products', productRoutes);
+
+
+app.listen(port, function(err){
+    if(err) throw err
+    console.log(`Server running on port ${port}`)
+})
+
